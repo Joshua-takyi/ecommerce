529e0d0f5f7113886a6cce33a4403ea9989000b2
joshua Takyi <takyijoshua019@gmail.com>
2025-01-19 12:22:50 +0000

    get users added

--- src/app/api/signin/route.js
+++ src/app/api/signin/route.js
@@ -1,4 +1,6 @@
+import { UserAuth } from "@/models/schema";
 import { SignInAction } from "@/server/action";
+import { connectDb } from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -14,6 +16,19 @@ export async function POST(req) {
 		return NextResponse.json({
 			message: "Sign in successful",
 			data: res,
+		});
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 400 });
+	}
+}
+
+export async function GET() {
+	try {
+		await connectDb();
+		const users = await UserAuth.find({});
+		return NextResponse.json({
+			message: "Sign in successful",
+			data: users,
 		});
 	} catch (error) {
 		return NextResponse.json({ error: error.message }, { status: 400 });
